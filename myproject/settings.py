a487a8360e95c626eabbda20f4695ce95415bfdb
Your NameVladimirs77nt <vladimirs77@yandex.ru>
2024-03-06 23:17:23 +0500

    домашнее задание 6

--- myproject/settings.py
+++ myproject/settings.py
@@ -19,14 +19,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u1nfd-=!60#9_^u#ga!07%qn)g*^e$%vs&3)6+p8$)_kvc+mvi'
+
+import os
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1', 
     '192.168.1.42',
+    'vladimirs77.pythonanywhere.com',
 ]
 
 # Application definition
@@ -78,8 +84,15 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vladimirs77$default',
+        'USER': 'vladimirs77$',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'vladimirs77.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -117,9 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
